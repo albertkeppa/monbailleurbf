0bdb6a70029627dfe2f43dc24ab4eef41538b87f
albertkeppa <albertkeppa@gmail.com>
2026-06-08 22:10:18 +0000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,18 +1,52 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { supabase } from '../lib/supabase'
+
 export default function Home() {
+  const [annonces, setAnnonces] = useState([])
+
+  useEffect(() => {
+    chargerAnnonces()
+  }, [])
+
+  async function chargerAnnonces() {
+    const { data, error } = await supabase
+      .from('annonces')
+      .select('*')
+      .order('created_at', { ascending: false })
+
+    if (!error) {
+      setAnnonces(data)
+    }
+  }
+
   return (
-    <main style={{
-      padding: "20px",
-      fontFamily: "Arial"
-    }}>
+    <main style={{ padding: '20px' }}>
       <h1>MonBailleurBF</h1>
 
-      <p>
-        Trouve rapidement une maison à louer ou à acheter au Burkina Faso.
-      </p>
+      <h2>Dernières annonces</h2>
 
-      <button>
-        Voir les annonces
-      </button>
+      {annonces.length === 0 ? (
+        <p>Aucune annonce disponible</p>
+      ) : (
+        annonces.map((annonce) => (
+          <div
+            key={annonce.id}
+            style={{
+              border: '1px solid #ddd',
+              padding: '10px',
+              marginBottom: '10px'
+            }}
+          >
+            <h3>{annonce.titre}</h3>
+            <p>{annonce.description}</p>
+            <p><b>Prix :</b> {annonce.prix} FCFA</p>
+            <p><b>Ville :</b> {annonce.ville}</p>
+            <p><b>Quartier :</b> {annonce.quartier}</p>
+          </div>
+        ))
+      )}
     </main>
-  );
-  }
+  )
+            }
