0cdd215c279492a5513f78229d4f506196c9133d
albertkeppa <albertkeppa@gmail.com>
2026-06-08 20:22:52 +0000

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <main style={{
+      padding: "20px",
+      fontFamily: "Arial"
+    }}>
+      <h1>MonBailleurBF</h1>
+
+      <p>
+        Trouve rapidement une maison à louer ou à acheter au Burkina Faso.
+      </p>
+
+      <button>
+        Voir les annonces
+      </button>
+    </main>
+  );
+  }
